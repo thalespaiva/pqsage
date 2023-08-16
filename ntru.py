0d3ac0e3470df7776256b83c30a07c28a4f9d200
Thales Paiva <thalespaiva@gmail.com>
2023-08-16 15:59:39 -0300

    Adds recovery of r and plaintext sanity check

--- ntru.py
+++ ntru.py
@@ -1,6 +1,6 @@
 from sage.all import *
 
-# from Crypto.Random import get_random_bytes
+from Crypto.Random import get_random_bytes
 
 from dataclasses import dataclass
 
@@ -8,12 +8,13 @@ from dataclasses import dataclass
 class NTRUHPS_Parameters:
     n: int
     q: int
+    d: int
 
 class NTRUHPS_PKE_CPA:
     SecurityParameters = {
-        1: NTRUHPS_Parameters(n=509, q=2048),
-        3: NTRUHPS_Parameters(n=677, q=2048),
-        5: NTRUHPS_Parameters(n=821, q=2048),
+        1: NTRUHPS_Parameters(n=509, q=2048, d=254),
+        3: NTRUHPS_Parameters(n=677, q=2048, d=254),
+        5: NTRUHPS_Parameters(n=821, q=2048, d=254),
     }
 
     def __init__(self, security_level):
@@ -80,9 +81,12 @@ class NTRUHPS_PKE_CPA:
     def from_Rq_to_Sq(self, poly):
         return self.Sq(self.centered(poly.mod(self.phi_1)))
 
+    def from_Rq_to_S3(self, poly):
+        return self.S3(self.centered(from_Rq_to_Sq(poly)))
+
     def keygen(self):
         f_in_s3 = self.S3(self.generate_ternary_coefficients(self.params.n - 2))
-        g_in_s3 = self.S3(self.generate_ternary_coefficients_for_d(self.params.q//8 - 2,
+        g_in_s3 = self.S3(self.generate_ternary_coefficients_for_d(self.params.d,
                                                                    self.params.n - 2))
         f_inv_3 = f_in_s3.inverse()
 
@@ -97,35 +101,44 @@ class NTRUHPS_PKE_CPA:
     def encrypt(self, pk, randomness):
         h = pk
         r = self.Rq(self.generate_ternary_coefficients(self.params.n - 2))
-        m = self.Rq(self.generate_ternary_coefficients_for_d(self.params.q // 8 - 2, self.params.n - 2))
+        m = self.Rq(self.generate_ternary_coefficients_for_d(self.params.d,
+                                                             self.params.n - 2))
 
         c = h * r + m
 
         m = self.centered(self.from_Rq_to_Sq(m))
+        r = self.centered(self.from_Rq_to_Sq(r))
         return (r, m), c
+
+    def is_valid_plaintext(self, r, m):
+        if (m.count(-1), m.count(1)) != (self.params.d//2, self.params.d//2):
+            return False
+
+        r_non_ternary_coefficients = len([c for c in r if c not in {-1, 0, 1}])
+        if r_non_ternary_coefficients:
+            return False
+
+        return True
 
     def decrypt(self, sk, ciphertext):
         (f, f_inv_q, f_inv_3, h_inv_q, g) = sk
         c = ciphertext
 
         if c.lift().mod(self.phi_1) != 0:
-            return (0, 0, 1)
+            return None
 
         a = (c * self.to_Rq(f))
         m = self.S3(self.centered(a)) * f_inv_3
+
+        r = (c - self.from_S3_to_Rq(m)) * h_inv_q
+
         m = self.centered(m)
-        return m
+        r = self.centered(self.from_Rq_to_Sq(r))
 
-
-# Table from NTRU specs for HPS parameters:
-# − n is a prime and both 2 and 3 are of order n − 1 in (Z/n)× ,
-# − p = 3,
-# − q is a power of two,
-# − Lf = T ,
-# − Lg = T (q/8 − 2),
-# − Lr = T ,
-# − Lm = T (q/8 − 2), and
-# − Lift is the identity m 7→ m.
+        if self.is_valid_plaintext(r, m):
+            return r, m
+        else:
+            return None
 
 
 def test_ntru():
@@ -136,8 +149,10 @@ def test_ntru():
         # randomness = get_random_bytes(32)
         message, ciphertext = ntru.encrypt(pk, None)
         decrypted = ntru.decrypt(sk, ciphertext)
-        assert decrypted == message[1]
+        assert decrypted == message
 
         print(f'NTRU {security_level}: PASSED')
-        # print(f'message = {"".join(map(str, message))}')
 
+
+if __name__ == '__main__':
+    test_ntru()
